071c32c028ef083b37f78ad3cbbd4613df116044
Seojoon Hwang <iamaloneinseoul@gmail.com>
2023-02-24 15:22:27 +0900

    no message

--- nav_bar/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.navBar__toggleBtn');
-const menu = document.querySelector('.navBar__menu');
-const icon = document.querySelector('.navBar__icon');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icon.classList.toggle('active');
-});
